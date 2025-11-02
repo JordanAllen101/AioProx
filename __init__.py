3fce704360b3c3d2318f5dafea6ec81a91eef6b6
JordanAllen101 <stoddardj08@gmail.com>
2025-11-02 01:05:33 -0500

    Sort source in same fashion as PyPi

--- __init__.py
+++
@@ -1,4 +0,0 @@
-from .core import Proxy, AsyncLoopThread
-
-__all__ = ["Proxy", "AsyncLoopThread"]
-__version__ = "0.1.5"
---
+++ aioprox/__init__.py
@@ -0,0 +1,4 @@
+from .core import Proxy, AsyncLoopThread
+
+__all__ = ["Proxy", "AsyncLoopThread"]
+__version__ = "0.1.5"
